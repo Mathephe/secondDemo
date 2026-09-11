0330b51620f46f6d91b22fbc8e3d25cc7ba2ed27
Neani Mulaudzi <neani.mulaudzi1@gmail.com>
2026-09-11 16:22:07 -0700

    Added close browser method

--- src/main/java/launchNdosiTest.java
+++ src/main/java/launchNdosiTest.java
@@ -4,9 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class launchNdosiTest {
     WebDriver driver;
 
+    @Test
     public void launchNdosiUrlTest(){
         driver = new ChromeDriver();
         driver.get("https://ndosisimplifiedautomation.vercel.app/");
         driver.manage().window().maximize();
+    }
+    @AfterTest
+    public void closeBrower()
+    {
+        driver.close();
     }
 }
